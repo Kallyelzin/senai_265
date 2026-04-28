fe128986b4a040180cb0ce71532c8a81638c8f0b
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-27 22:08:56 -0300

    FIM aula 3

---
+++ usuario.py
@@ -0,0 +1,10 @@
+login = "pedro23"
+senha = "1234"
+
+login = input("Qual seu user name? ")
+senha = input("Qual sua senha? ")
+
+if login == "pedro23" and senha=="1234":
+    print("Logando!") 
+else:
+    print("Login incorreto, tente novamente!")
