aa9fa4384871a62e26b00ddf076c6e0a6a193177
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-27 21:26:17 -0300

    Sinaleiro

--- lacos.py
+++ lacos.py
@@ -1,1 +1,11 @@
-print("Salve")
+sinal = input("qual o sinal atual?")
+
+if sinal == "vermelho":
+    print("PARE!")
+elif sinal == "amarelo":
+    print("ATENÇÂO!")
+elif sinal == "verde":
+    print("PODE PASSAR!")
+else:
+    print("Sinal não identificado!")
+    
