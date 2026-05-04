bfc2a34cd307d0e6b136e9c1c15ea8511e7c81af
Ryo <yagomigliorinidossantoskallyel@gmail.com>
2026-05-04 20:47:24 -0300

    FIM

---
+++ idade.py
@@ -0,0 +1,10 @@
+idade = int(input("Qual sua idade?: ")
+
+if idade < 12:
+      print("Criança")
+else idade < 18:
+      print("Adolecente")
+else idade < 60:
+      print("Adulto")
+elif:
+      print("Idoso")
