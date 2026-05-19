eaceecb294379d0070256a90d90f953d5e9f1d23
Ryo <yagomigliorinidossantoskallyel@gmail.com>
2026-05-18 21:58:26 -0300

    finish
    Removed champion search functionality and added print statement for each champion.

--- lista-alunos.py
+++ lista-alunos.py
@@ -1,11 +1,4 @@
-print("champions in top 5")
-print("TOP 1 ItzRealMe, TOP 2 Marloww, TOP 3 ClownPierce, TOP 4 JudeLow, TOP 5 Ryo")
-ps_champions = input("What this champion you search? ")
 champions = ["Ryo", "Marloww", "Clownpierce", "Judelow", "TtzRealMe"]
 
 for i in champions:
-    if i == ps_champions:
-        print("This champion is on!")
-        break
-else:
-    print("This champion is of")
+    print(champrion)
